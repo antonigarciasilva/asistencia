271d3e13c855b58c1f93a72af5b293032bc440d3
Antoni Garcia Silva <ags13dxags@gmail.com>
2022-04-11 11:09:20 -0500

    Validaciones de números y letras ok

---
+++ admin/includes/cashadvance_modal.php
@@ -0,0 +1,96 @@
+<!-- Add -->
+<div class="modal fade" data-backdrop="static" data-keyboard="false"  id="addnew">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b>Agregar Adelanto de Efectivo</b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="cashadvance_add.php">
+          		  <div class="form-group">
+                  	<label for="employee"  class="col-sm-3 control-label">DNI Empleado</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" onKeypress="ValidarNumeros()"  id="employee" name="employee" required>
+                  	</div>
+                </div>
+                <div class="form-group">
+                    <label for="amount" class="col-sm-3 control-label">Monto</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control"  onKeypress="ValidarNumeros()" id="amount" name="amount" required>
+                    </div>
+                </div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Guardar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<!-- Edit -->
+<div class="modal fade" data-backdrop="static" data-keyboard="false" id="edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b><span class="date"></span> - <span class="employee_name"></span></b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="cashadvance_edit.php">
+            		<input type="hidden" class="caid" name="id">
+                <div class="form-group">
+                    <label for="edit_amount" class="col-sm-3 control-label">Monto</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control"  onKeypress="ValidarNumeros()" id="edit_amount" name="amount" required>
+                    </div>
+                </div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Actualizar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<!-- Delete -->
+<div class="modal fade" data-backdrop="static" data-keyboard="false" id="delete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b><span class="date"></span></b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="cashadvance_delete.php">
+            		<input type="hidden" class="caid" name="id">
+            		<div class="text-center">
+	                	<p>Eliminar Adelanto de Efectivo</p>
+	                	<h2 class="employee_name bold"></h2>
+	            	</div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Eliminar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<script>
+	function ValidarNumeros() {
+		if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;
+	}
+</script>
+     
---
+++ admin/includes/position_modal.php
@@ -0,0 +1,125 @@
+<!-- Add -->
+<div class="modal fade" data-backdrop="static" data-keyboard="false" id="addnew">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b>Agregar Cargo</b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="position_add.php">
+          		  <div class="form-group">
+                  	<label for="title" class="col-sm-3 control-label">Nombre Cargo</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" onkeypress="return soloLetras(event)"  id="title" name="title" required>
+                  	</div>
+                </div>
+                <div class="form-group">
+                    <label for="rate" class="col-sm-3 control-label">Pago por Hora</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" onKeypress="ValidarNumeros()" id="rate" name="rate" required>
+                    </div>
+                </div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Guardar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<!-- Edit -->
+<div class="modal fade"  data-backdrop="static" data-keyboard="false" id="edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b>Actualizar Cargo</b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="position_edit.php">
+            		<input type="hidden" id="posid" name="id">
+                <div class="form-group">
+                    <label for="edit_title" class="col-sm-3 control-label">Nombre del Cargo</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" onkeypress="return soloLetras(event)" id="edit_title" name="title">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="edit_rate" class="col-sm-3 control-label">Costo por Hora</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" onKeypress="ValidarNumeros()" id="edit_rate" name="rate">
+                    </div>
+                </div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Actualizar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<!-- Delete -->
+<div class="modal fade"  data-backdrop="static" data-keyboard="false" id="delete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b>Eliminando...</b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="position_delete.php">
+            		<input type="hidden" id="del_posid" name="id">
+            		<div class="text-center">
+	                	<p>ELIMINAR POSICIÓN</p>
+	                	<h2 id="del_position" class="bold"></h2>
+	            	</div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
+            	<button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Eliminar</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
+
+<script>
+	function ValidarNumeros() {
+		if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;
+	}
+
+
+	
+	function soloLetras(e) {
+    var key = e.keyCode || e.which,
+      tecla = String.fromCharCode(key).toLowerCase(),
+      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz",
+      especiales = [8, 37, 39, 46],
+      tecla_especial = false;
+
+    for (var i in especiales) {
+      if (key == especiales[i]) {
+        tecla_especial = true;
+        break;
+      }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+      return false;
+    }
+  }
+</script>
+     
+     
